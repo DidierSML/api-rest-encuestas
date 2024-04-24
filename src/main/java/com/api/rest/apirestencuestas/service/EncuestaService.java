df39bcdde33fc3e7779d96f2ff09498480739221
DidierSML <didiersmaldonad@gmail.com>
2024-04-24 11:07:20 -0500

    Working on -Encuesta´s- (ServiceImpl & Controller)  Layers

--- src/main/java/com/api/rest/apirestencuestas/service/EncuestaService.java
+++ src/main/java/com/api/rest/apirestencuestas/service/EncuestaService.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 public interface EncuestaService {
 
-    EncuestaDto createUser (EncuestaRequest encuestaRequest);
+    EncuestaDto createEncuesta (EncuestaRequest encuestaRequest);
 
     List <EncuestaDto> getAllEncuestas ();
 
