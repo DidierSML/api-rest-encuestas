37329dfac67e7b30f20203bfec53ab090b8bc2c4
DidierSML <didiersmaldonad@gmail.com>
2024-05-11 11:37:37 -0500

    Working on -updateOpcionesByEncuestaId- Service Layer

--- src/main/java/com/api/rest/apirestencuestas/service/OpcionService.java
+++ src/main/java/com/api/rest/apirestencuestas/service/OpcionService.java
@@ -1,11 +1,14 @@
 package com.api.rest.apirestencuestas.service;
 
 import com.api.rest.apirestencuestas.dto.OpcionDto;
+import com.api.rest.apirestencuestas.dto.request.OpcionRequest;
 
 import java.util.Set;
 
 public interface OpcionService {
 
    Set<OpcionDto> getOpcionesByEncuestaId (Long encuestaId);
+
+   Set <OpcionDto> updateOpcionesByEncuestaId (Long encuestaId, Long opcionId, OpcionRequest opcionRequest);
 
 }
