fa1aa18a7a2d86b9e91db936b9b342e18230f4a8
DidierSML <didiersmaldonad@gmail.com>
2024-06-05 17:30:25 -0500

    Refactoring

--- src/main/java/com/api/rest/apirestencuestas/dto/mapper/MapperOpcion.java
+++ src/main/java/com/api/rest/apirestencuestas/dto/mapper/MapperOpcion.java
@@ -5,6 +5,7 @@ import com.api.rest.apirestencuestas.dto.request.OpcionRequest;
 import com.api.rest.apirestencuestas.dto.response.OpcionResponse;
 import com.api.rest.apirestencuestas.model.Opcion;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
 import java.util.Set;
@@ -18,9 +19,10 @@ public interface MapperOpcion {
 
     OpcionDto fromEntityToDto (Opcion opcion);
 
-    Set <OpcionDto> fromEntityListToDtoList (Set <Opcion> opcion);
-
+    @Mapping(target = "value", source = "value")
     OpcionResponse fromDtoToResponse (OpcionDto opcionDto);
+
+    Set <OpcionDto> fromEntityListToDtoList (Set <Opcion> opcion);
 
     Set <OpcionResponse> fromDtoListToResponseList (Set <OpcionDto> opcionDtoList);
 
